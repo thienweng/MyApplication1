a505abe7fecd303a8f0460896b31183b0c40e983
taruc <tantw-pm17@student.tarc.edu.my>
2020-03-05 12:38:42 +0800

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,26 +3,50 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dice_image: ImageView
+    lateinit var dice_image2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+         dice_image = findViewById(R.id.dice_image)
+        dice_image2 = findViewById(R.id.dice_image_2)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {rollDice()}
+        rollButton.setOnClickListener { rollDice() }
+
 
     }
+
     private fun rollDice() {
-        val rollText:TextView =findViewById(R.id.roll_text)
-        val randomInt = (1..6).random()
-        rollText.text = randomInt.toString()
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+
+
+        dice_image.setImageResource(imagerand())
+        dice_image2.setImageResource(imagerand())
     }
 
+    private fun imagerand(): Int {
+        val randomInt = (1..6).random()
+      return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
+    }
 }
+
+
+
+
+
+
